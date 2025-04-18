a26e65b27fd25cc031808ef38a4b7f0d17dcf542
Maria Bogomaz <bogomazm@gmail.com>
2025-04-18 09:08:58 -0600

    Fix styles for the About Me section

--- storyblok/AboutMe.vue
+++ storyblok/AboutMe.vue
@@ -1,5 +1,5 @@
-<template lang="html">
-  <div v-editable="blok" class="bg-gradient-to-b from-white to-[#E8EFF5] py-2">
+<template>
+  <div v-editable="blok" class="bg-gradient-to-b from-white to-[#E8EFF5] pt-2 pb-8">
     <div class="max-w-3xl mx-auto px-4 mb-10">
       <div class="text-center">
         <h2 class="text-4xl text-[#718FCB] font-['Mrs_Saint_Delafield'] mb-4">{{blok.title}}</h2>
@@ -8,8 +8,8 @@
     </div>
     <blockquote class="max-w-3xl mx-auto px-4 py-5 bg-white shadow-md relative min-h-[120px] mb-10">
       <p class="text-center text-2xl leading-relaxed max-w-[80%] mx-auto">{{blok.quote}}</p>
-      <span class="absolute top-2 left-2 text-6xl text-[#718FCB] opacity-50">"</span>
-      <span class="absolute bottom-[-110px] right-[-32px] text-[25em] text-[#718FCB] opacity-50">"</span>
+      <span class="absolute top-2 left-2 text-6xl text-[#718FCB] opacity-50 font-['Mrs_Saint_Delafield']">"</span>
+      <span class="absolute bottom-[-250px] right-[-32px] text-[25em] text-[#718FCB] opacity-50 font-['Mrs_Saint_Delafield']">"</span>
     </blockquote>
     <div class="max-w-3xl mx-auto px-4">
       <div class="prose max-w-none" v-html="renderedText2"></div>
